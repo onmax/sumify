20d30dbaa2c2dcbcabd991dc59e3fd9651a66346
maximo <maximo@nimiq.com>
2023-03-02 22:32:28 +0100

    CI

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('my test', () => {
+    it('should pass CI', () => {
+        expect(1).toBe(1)
+    })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+export const hello = () => {
 console.log('Hello World!');
+}
